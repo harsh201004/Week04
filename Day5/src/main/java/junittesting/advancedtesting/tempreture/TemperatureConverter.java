25eece4f44fa154c8bf367a017b8dadbf2f6aa4f
harsh201004 <harshraghuwanshi2010@gmail.com>
2025-02-16 22:53:57 +0530

    [Harsh]Add. Solution to junit testing for tempreture

---
+++ Day5/src/main/java/junittesting/advancedtesting/tempreture/TemperatureConverter.java
@@ -0,0 +1,15 @@
+package junittesting.advancedtesting.tempreture;
+
+public class TemperatureConverter {
+
+    // Method to convert Celsius to Fahrenheit
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
+    // Method to convert Fahrenheit to Celsius
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+}
+
