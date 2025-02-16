7735f6678b140d6e0814fe782ce3b43eeb422e8e
Harsh Raghuwanshi <119292911+harsh201004@users.noreply.github.com>
2025-02-16 23:11:03 +0530

    Ini

--- Day1/src/main/java/Generics/dynamiconlinemarketplace/ClothingCategory.java
+++
@@ -1,8 +0,0 @@
-package dynamiconlinemarketplace;
-
-//Clothing Category Class
-public class ClothingCategory extends Category{
-    public ClothingCategory(String category, double price){
-        super(category, price);
-    }
-}
