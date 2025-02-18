3eaf693b86c611ab9b506844ee489f409f4116b7
harsh201004 <harshraghuwanshi2010@gmail.com>
2025-02-18 11:23:58 +0530

    [Harsh]add. Solution to perform the math operations

---
+++ Day6/src/main/java/reflection/intermediatelevel/MathOperations.java
@@ -0,0 +1,18 @@
+package reflection.intermediatelevel;
+
+public class MathOperations {
+    // Add method
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Subtract method
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Multiply method
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
