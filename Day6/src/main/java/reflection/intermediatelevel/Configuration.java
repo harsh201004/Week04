5f08b67a239d54d313eebb881443e04ba2867d9f
harsh201004 <harshraghuwanshi2010@gmail.com>
2025-02-18 11:23:33 +0530

    [Harsh]add. Solution to print the private key value

---
+++ Day6/src/main/java/reflection/intermediatelevel/Configuration.java
@@ -0,0 +1,11 @@
+package reflection.intermediatelevel;
+
+public class Configuration {
+    // Private static field
+    private static String API_KEY = "initial_api_key";
+
+    // Method to print the API_KEY value
+    public static void printAPIKey() {
+        System.out.println("API Key: " + API_KEY);
+    }
+}
