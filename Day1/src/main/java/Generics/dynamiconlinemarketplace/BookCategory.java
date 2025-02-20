0ddf69bff8c7aa9fe57c94712a1e11d2b6003812
Harsh Raghuwanshi <119292911+harsh201004@users.noreply.github.com>
2025-02-20 15:49:32 +0530

    ‎

--- Day1/src/main/java/Generics/dynamiconlinemarketplace/BookCategory.java
+++
@@ -1,9 +0,0 @@
-package dynamiconlinemarketplace;
-
-//BookCategory Class
-public class BookCategory extends Category{
-        public BookCategory(String category, double price){
-            super(category, price);
-        }
-}
-
--- Day1/src/main/java/Generics/universitycoursemanagementsystem/AssignmentCourse.java
+++
@@ -1,8 +0,0 @@
-package universitycoursemanagementsystem;
-
-//Concrete CLass Assignment Course
-public class AssignmentCourse extends CourseType {
-    public AssignmentCourse(String courseName) {
-        super(courseName);
-    }
-}
